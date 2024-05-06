c47bf25aeb351aaaaa191b893d80ee173487ebfd
Shestakov Artem <artem.shestakov@fleetcor.ru>
2024-05-06 12:44:31 +0300

    Модернизация скрипта сборки

--- Build.cmd
+++ Build.cmd
@@ -3,4 +3,13 @@
 IF EXIST build RMDIR /Q /S build
 
 cmake . -B build
+IF %errorlevel% neq 0 EXIT /b %errorlevel%
+
 cmake --build build --config Release
+IF %errorlevel% neq 0 EXIT /b %errorlevel%
+
+IF NOT EXIST deploy MKDIR deploy
+
+COPY /Y build\bin\Release\Migrator.exe deploy
+COPY /Y build\bin\Release\Signer.exe deploy
+DIR deploy
